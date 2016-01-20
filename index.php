5044d66cd54cd98863fde393a8b9d3791c3c427f
Master <oto@geolab.edu.ge>
2016-01-20 22:37:46 +0400

    update

--- index.php
+++ index.php
@@ -13,21 +13,37 @@ $client->addScope(array(
 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
-  $drive_service = new Google_Service_Gmail($client);
-  $files_list = $drive_service->users_messages->listUsersMessages('me');
+  $gmail_service = new Google_Service_Gmail($client);
+  $files_list = $gmail_service->users_messages->listUsersMessages('me',['q'=>'from:mailer-daemon@googlemail.com']);
   $messages = array();
   if ($files_list->getMessages()) {
         $messages = array_merge($messages, $files_list->getMessages());
         $pageToken = $files_list->getNextPageToken();
       }
   foreach($messages as $message){
-    // var_dump($message->getId());
+    var_dump($message->getId());
+    $getMessage = $gmail_service->users_messages->get('me',$message->getId(),['fields'=>'payload/body/data']);
+    // var_dump($getMessage->getPayLoad()['body']['data']);
+    $a = base64_decode($getMessage->getPayLoad()['body']['data']);
+    // echo $a;
+    $b  = strpos($a,'Message-ID:');
+    $c = $b+11;
+    // var_dump(strpos($a,'Message-ID:'));
+    $d =  trim(substr($a,$c,( strpos($a, PHP_EOL, $c) -$c)));
+    var_dump($d);
+    continue;
+    $getMessage2 = $gmail_service->users_messages->listUsersMessages('me',['q'=>'rfc822msgid:'.$d,'fields'=>'messages/id']);
+    $e = ($getMessage2->getMessages()[0]['id']);
+    $getMessage3 = $gmail_service->users_messages->get('me',$e,['fields'=>'payload/headers']);
+    // var_dump($getMessage3['payload']['headers']);
+    foreach ( $getMessage3['payload']['headers'] as $message ){
+      if ($message['name'] == 'To'){
+        var_dump($message['value']);
+        break;
+      }
+    }
   }
-  $getMessage = $drive_service->users_messages->get('me','14f8ec4149d06e07',['fields'=>'payload/body/data']);
-  // var_dump($getMessage->getPayLoad()['body']['data']);
-  $a = $getMessage->getPayLoad()['body']['data'];
-  echo base64_decode($a);
-  var_dump(strpos($a,'Message-ID'));
+
   // echo json_encode($files_list);
 } else {
   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/gmail/oauth2callback.php';
